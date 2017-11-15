16c323f48596c0e36541427270c3655c81808843
Justin Tooley <jstooley823@gmail.com>
2017-11-15 03:19:57 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,8 @@ class ApplicationController < Sinatra::Base
 	end
 
 	post "/signup" do
-		user = User.new(:username => params[:username], :password => params[:password])
+		user = User.new(:username => params[:username], 
+		:password => params[:password])
 	end
 
 
